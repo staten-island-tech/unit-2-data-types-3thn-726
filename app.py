c001b46f7e505143f9423b0bcbf69db6899adcd3
3thn-726 <not3thn@gmail.com>
2025-02-04 10:14:57 -0500

    commmit

---
+++ app.py
@@ -0,0 +1,39 @@
+#Strings are for representing characters, names, words, etc.
+#name= "deivid"
+#integers represent whole numbers
+#age: 14
+#Floats represents decimals
+#wallet: 5.45
+#booleans represents true/false, used in evaluations
+#graduated = False
+
+""" def add(x,y):
+    print(x+y) """
+#input asks the user a question and stores their response as a value
+#bill = float(input("How much did the bill cost?"))
+#print(type(bill))
+#add(40, bill)
+
+#Lists
+#students = ("Joanna", "Deivid", "David", "Other David", "Ethan")
+#similar to saying for i in range(5): print(students[i])
+""" print(students[4])
+for student in students:
+    print(student) """
+
+""" moneys = [1,2,3,4,5,6]
+total: 0
+for money in moneys:
+ """
+
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) """
+
+print(values[0])
+print(values[6])
